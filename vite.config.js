518355108973e26a17efc746e350492e4c743e75
cfchen_tku <cfchen@mail.tku.edu.tw>
2026-04-16 16:19:13 +0800

    1.113

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 如果您要將網站部署到根目錄 (例如 https://example.com/)，請將 base 設為 '/'
-  base: '/',
+  // 部署到 GitHub Pages 子目錄，請將 base 設為 '/儲存庫名稱/' (前後都要有斜線)
+  base: '/20260416/',
 })
