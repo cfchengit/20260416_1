13f7d40b857cb4f3707be9795061389df2e8fb15
cfchen_tku <cfchen@mail.tku.edu.tw>
2026-04-16 15:55:38 +0800

    重新佈署1.2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/20260416/',
+  // 如果您要將網站部署到根目錄 (例如 https://example.com/)，請將 base 設為 '/'
+  base: '/',
 })
